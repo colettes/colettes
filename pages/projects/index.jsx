360dd3c37d94b69ffcb3c7ab2bc08edc8b5e9be9
Colette Smith <colette11295@gmail.com>
2022-04-07 19:37:41 -0500

    Update project info

--- pages/projects/index.jsx
+++ pages/projects/index.jsx
@@ -7,32 +7,26 @@ const projectList = [
     image: "/projectPics/stockTrackerApp.png",
     path: "/projects/stocktracker",
     link: "https://www.behance.net/gallery/115517541/Stock-Tacker-App/modules/659203887",
-    info: "A fullstack and full scale web redesign of system to manage group projects and assignments.",
+    info: "TODO",
   },
   {
     title: "Design: Logo Design",
     image: "/projectPics/tetiLogo.png",
     path: "/projects/tetilogo",
     link: "https://www.behance.net/gallery/133423139/Logo-Design",
-    info: "A fullstack and full scale web redesign of system to manage group projects and assignments.",
+    info: "TODO",
   },
   {
     title: "Fullstack: D2L Redesign",
     image: "/projectPics/d3l.png",
     path: "projects/d3l",
-    info: "A fullstack and full scale web redesign of system to manage group projects and assignments.",
+    info: "TODO",
   },
   {
     title: "UXD: Class Scheduler",
     image: "/projectPics/classscheduler",
     path: "projects/classScheduler",
-    info: "A fullstack and full scale web redesign of system to manage group projects and assignments.",
-  },
-  {
-    title: "Fullstack: Art Application",
-    image: "/projectPics/artApplication",
-    path: "projects/artapp",
-    info: "A fullstack and full scale web redesign of system to manage group projects and assignments.",
+    info: "TODO",
   },
 ];
 
