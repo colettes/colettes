464d5044e47bccaf8affe2acd71d466cd421df88
Colette Smith <colette11295@gmail.com>
2022-04-27 20:30:30 -0500

    Style index page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,20 @@
-import styles from "../styles/Home.module.css";
-import "bootstrap-icons/font/bootstrap-icons.css";
+import styles from '../styles/Home.module.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 export default function Index() {
-  return (
-    <div className={styles.container}>
-      <h1>
-        Hi there,
-        <br />
-        my name is <b>Colette.</b>
-      </h1>
-    </div>
-  );
+    return (
+        <div className={styles.container}>
+            <h1>
+                Hi there!
+                <br />
+                My name is <b>Colette.</b>
+            </h1>
+            <h2>
+                I like to <b>code</b> things, <br />
+                <b>design</b> stuff, <br />
+                <b>manage</b> my business, <br />
+                and read about mezcal &#38; wine.
+            </h2>
+        </div>
+    );
 }
