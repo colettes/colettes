9f40da726d49b50a2e7443fadf535814e566c64a
Colette Smith <colette11295@gmail.com>
2022-04-27 20:30:18 -0500

    Style resume page

--- pages/resume.jsx
+++ pages/resume.jsx
@@ -1,58 +1,125 @@
-import Image from "next/image";
-import styles from "../styles/Resume.module.css";
+import Image from 'next/image';
+import styles from '../styles/Resume.module.css';
 
 function ResumePage() {
-  return (
-    <div className={styles.resume}>
-      <h1>Resume</h1>
-      <div className={styles.section}>
-        <div className={styles.section_title}>Work Experience</div>
-        <div className={styles.subsection}>
-          <div className={styles.date}>2013 &darr; Now</div>
-          <div className={styles.subsection_info}>
-            <div className={styles.subsection_title}>Teti Vintage</div>
-            <div className={styles.subsection_subtitle}>Founder</div>
-            <div className={styles.subsection_desc}>
-              In high school, I turned my hobby into a small business which I
-              have maintained for several years. I continually provide excellent
-              customer service with over 400 5-star reviews across multiple
-              platforms through positive engagement and timely responses to
-              customer needs.
+    const techSkills = [
+        'Javascript / React.js',
+        'Java',
+        'HTML/CSS',
+        'GIT Version Control',
+        'Object-Oriented Programming',
+        'Functional Programming',
+        'AdobeXD Wire Framing',
+    ];
+    const softSkills = [
+        'Customer service',
+        'Managing client expectations',
+        'Strong verbal communication',
+        'Prioritization',
+    ];
+
+    return (
+        <div className={styles.resume}>
+            <h1>Resume</h1>
+            <div className={styles.section}>
+                <div className={styles.section_title}>Work Experience</div>
+                <div className={styles.subsection}>
+                    <div className={styles.date}>2013 &darr; Now</div>
+                    <div className={styles.subsection_info}>
+                        <div className={styles.subsection_title}>
+                            Teti Vintage
+                        </div>
+                        <div className={styles.subsection_subtitle}>
+                            Founder
+                        </div>
+                        <div className={styles.subsection_desc}>
+                            In high school, I turned my hobby into a small
+                            business which I have maintained for several years.
+                            I continually provide excellent customer service
+                            with over 400 5-star reviews across multiple
+                            platforms through positive engagement and timely
+                            responses to customer needs.
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.subsection}>
+                    <div className={styles.date}>2017 &darr; Now</div>
+                    <div className={styles.subsection_info}>
+                        <div className={styles.subsection_title}>
+                            Centro Restaurant Group
+                        </div>
+                        <div className={styles.subsection_subtitle}>
+                            Bartender / Manager
+                        </div>
+                        <div className={styles.subsection_desc}>
+                            I maintain an important role in elevating customer
+                            satisfaction by remaining calm and calibrated under
+                            demanding situations. I lead and direct employees to
+                            address current business needs in a fast-paced
+                            environment.
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.subsection}>
+                    <div className={styles.date}>2020 &darr; 2021</div>
+                    <div className={styles.subsection_info}>
+                        <div className={styles.subsection_title}>
+                            Radford J. Smith, Chtrd.
+                        </div>
+                        <div className={styles.subsection_subtitle}>
+                            Office Assistant
+                        </div>
+                        <div className={styles.subsection_desc}>
+                            Provided direct assistance to the office manager by
+                            handling confidential information and following
+                            strict deadlines. I was required to pay accute
+                            attention to detail in order to accurately and
+                            fairly bill each client.
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
+            <div className={styles.section}>
+                <div className={styles.section_title}>Education</div>
+                <div className={styles.subsection}>
+                    <div className={styles.date}>Jun 2021</div>
+                    <div className={styles.subsection_info}>
+                        <div className={styles.subsection_title}>
+                            DePaul University
+                        </div>
+                        <div className={styles.subsection_subtitle}>
+                            B.S. Computer Science <br /> Concentration: Software
+                            Development
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.section}>
+                <div className={styles.section_title}>Skills</div>
+                <div className={styles.subsection}>
+                    <div className={styles.subsection_skills}>
+                        <div className={styles.subsection_skills_title}>
+                            Tech Skills
+                            {techSkills.map((skill) => (
+                                <div className={styles.subsection_skill}>
+                                    {skill}
+                                </div>
+                            ))}
+                        </div>
+
+                        <div className={styles.subsection_skills_title}>
+                            Soft Skills
+                            {softSkills.map((skill) => (
+                                <div className={styles.subsection_skill}>
+                                    {skill}
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className={styles.subsection}>
-          <div className={styles.date}>2017 &darr; Now</div>
-          <div className={styles.subsection_info}>
-            <div className={styles.subsection_title}>
-              Centro Restaurant Group
-            </div>
-            <div className={styles.subsection_subtitle}>
-              Bartender / Manager
-            </div>
-            <div className={styles.subsection_desc}>
-              I maintain an important role in elevating customer satisfaction by
-              sustaining a positive disposition under demanding situations. I
-              lead employees to address current business needs in a fast-paced
-              environment.
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className={styles.section}>
-        <div className={styles.section_title}>Education</div>
-        <div className={styles.subsection}>
-          <div className={styles.date}>Jun 2021</div>
-          <div className={styles.subsection_info}>
-            <div className={styles.subsection_title}>DePaul University</div>
-            <div className={styles.subsection_subtitle}>
-              B.S. Computer Science
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default ResumePage;
