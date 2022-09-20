ae920fff1cab4b0daed7070bf32e9a860d16d50a
Colette Smith <colette11295@gmail.com>
2022-09-20 16:45:25 -0500

    Change main colors and font

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,29 +1,33 @@
-import styles from "../styles/Layout.module.css";
-import Head from "next/head";
-import NavBar from "./NavBar";
-import Link from "next/link";
-import Footer from "./Footer";
+import styles from '../styles/Layout.module.css';
+import Head from 'next/head';
+import NavBar from './NavBar';
+import Link from 'next/link';
+import Footer from './Footer';
 
 export default function Layout(props) {
-  return (
-    <div className={styles.layout}>
-      <div>
-        <Head>
-          <title>Colette Smith</title>
-          <meta name="description" content="Colette's Portfolio" />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-      </div>
-      <main className={styles.main}>
-        <Link href="/">
-          <a className={styles.title}>Colette Smith</a>
-        </Link>
-        <NavBar />
-      </main>
-      <div className={styles.content}>
-        {props.children}
-        <Footer />
-      </div>
-    </div>
-  );
+    return (
+        <div className={styles.layout}>
+            <div>
+                <Head>
+                    <title>Colette Smith</title>
+                    <meta name="description" content="Colette's Portfolio" />
+                    <link rel="icon" href="/favicon.ico" />
+                    <link
+                        rel="stylesheet"
+                        href="https://use.typekit.net/scg1het.css"
+                    />
+                </Head>
+            </div>
+            <main className={styles.main}>
+                <Link href="/">
+                    <a className={styles.title}>Colette Smith</a>
+                </Link>
+                <NavBar />
+            </main>
+            <div className={styles.content}>
+                {props.children}
+                <Footer />
+            </div>
+        </div>
+    );
 }
